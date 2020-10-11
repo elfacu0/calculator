badc8e26716067e8e5ada6206c31db0f0bc3524e
k- <mahat.mejor@gmail.com>
2020-10-11 10:29:22 -0500

    Updates

---
+++ precache-manifest.c0cb7187708632b006deb5f8a1357d8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cbe8e20a931796bd90e57fa5baa7cd2",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "eed450282761f4bd7354",
+    "url": "/calculator/static/css/main.b2a151f9.chunk.css"
+  },
+  {
+    "revision": "3002905ffc683d91856d",
+    "url": "/calculator/static/js/2.d6d2a280.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.d6d2a280.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eed450282761f4bd7354",
+    "url": "/calculator/static/js/main.6189dfa5.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
